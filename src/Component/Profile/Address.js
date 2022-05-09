8bf6970e9de481e087a3f72f62aa63a394e75821
Ihssen Guessmi <ihssenguessmi@gmail.com>
2022-05-09 23:03:28 +0100

    first commit

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,11 @@
+function Info () {
+    return (
+        <>
+        <h4>Adress: Manouba</h4>
+        <h4>GitHub: https://github.com/IhssenGuessmi</h4>
+        <h4>Email: email.email@email.com</h4>
+        <h4>Phone number: 50 000 000</h4>
+        </>
+    )
+}
+export default Info
---
+++ src/Component/Profile/FullName.js
@@ -0,0 +1,6 @@
+function FullName () {
+    return (
+        <h3>Ihssen Guessmi</h3>
+    )
+}
+export default FullName
